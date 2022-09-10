3e14ad81f1ff56ec505c6755946c8b24fd355781
Sebastian <isebasus@gmail.com>
2022-09-10 15:18:44 -0700

    fix runtime errors

--- src/main/kotlin/us/isebas/compass/CompassApplication.kt
+++ src/main/kotlin/us/isebas/compass/CompassApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Compass
+class CompassApplication
 
 fun main(args: Array<String>) {
-	runApplication<Compass>(*args)
+	runApplication<CompassApplication>(*args)
 }
